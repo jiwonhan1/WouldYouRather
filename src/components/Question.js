03d0a773e380798aed075053f362f75f48b7b4d0
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-02-01 18:06:33 -0800

    Question page

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-const Question = (props) => {};
+const Question = (props) => {
+  const dispatch = useDispatch();
+  const id = props.match.params.id;
+  console.log(id);
+  return <React.Fragment></React.Fragment>;
+};
 
 export default Question;
